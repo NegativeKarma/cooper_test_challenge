9c9069d55145dbeb71393107b5107f518b31b598
Francesco <francesco.falchi@gmail.com>
2017-11-11 14:10:54 +0100

    Create descriptions for athlete

--- spec/athlete_spec.js
+++ spec/athlete_spec.js
@@ -1,6 +1,24 @@
 describe('Athlete', function(){
   var athlete;
 
-  
+  beforeEach(function(){
+    athlete = new Athlete({
+      gender: female,
+      age: 22,
+      completed_distance: 2500,
+    });
+  });
 
-})
+  it('should have a female gender', function(){
+    expect(athlete.gender).toEqual('female');
+  });
+
+  it('should have an age of 22', function(){
+    expect(athlete.age).toEqual(22);
+  });
+
+  it('should have a completed distance of 2500', function(){
+    expect(athlete.completed_distance).toEqual(2500);
+  });
+
+});
