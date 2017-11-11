126149d48a7323f84203498c05390501103e8d23
Francesco <francesco.falchi@gmail.com>
2017-11-11 20:02:09 +0100

    Beautify everything

--- spec/cooper_spec.js
+++ spec/cooper_spec.js
@@ -1,23 +1,43 @@
-describe('CooperResponse', function(){
+describe('CooperResponse', function() {
 
-  it('calculates Cooper test result and returns "Poor"', function(){
-    expect(CooperResponse({gender: 'female', age: 22, completed_distance: 1400})).toEqual('Poor');
+  it('calculates Cooper test result and returns "Poor"', function() {
+    expect(CooperResponse({
+      gender: 'female',
+      age: 22,
+      completed_distance: 1400
+    })).toEqual('Poor');
   });
 
-  it('calculates Cooper test result and returns "Average"', function(){
-    expect(CooperResponse({gender: 'female', age: 22, completed_distance: 1900})).toEqual('Average');
+  it('calculates Cooper test result and returns "Average"', function() {
+    expect(CooperResponse({
+      gender: 'female',
+      age: 22,
+      completed_distance: 1900
+    })).toEqual('Average');
   });
 
-  it('calculates Cooper test result and returns "Excellent"', function(){
-    expect(CooperResponse({gender: 'male', age: 42, completed_distance: 2500})).toEqual('Excellent');
+  it('calculates Cooper test result and returns "Excellent"', function() {
+    expect(CooperResponse({
+      gender: 'male',
+      age: 42,
+      completed_distance: 2500
+    })).toEqual('Excellent');
   });
 
-  it('calculates Cooper test result and returns "Average"', function(){
-    expect(CooperResponse({gender: 'male', age: 73, completed_distance: 1721})).toEqual('Average');
+  it('calculates Cooper test result and returns "Average"', function() {
+    expect(CooperResponse({
+      gender: 'male',
+      age: 73,
+      completed_distance: 1721
+    })).toEqual('Average');
   });
 
-  it('calculates Cooper test result and returns "Above Average"', function(){
-    expect(CooperResponse({gender: 'male', age: 15, completed_distance: 2628})).toEqual('Above Average');
+  it('calculates Cooper test result and returns "Above Average"', function() {
+    expect(CooperResponse({
+      gender: 'male',
+      age: 15,
+      completed_distance: 2628
+    })).toEqual('Above Average');
   });
 
 });
--- src/athlete.js
+++ src/athlete.js
@@ -1,4 +1,4 @@
-function Athlete(attr){
+function Athlete(attr) {
   this.gender = attr.gender;
   this.age = attr.age;
   this.completed_distance = attr.completed_distance;
